dd81c21208654834a1920f38afc5c35360ae1416
SAhmed28 <72643620+SAhmed28@users.noreply.github.com>
2022-04-12 20:21:27 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,117 @@
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('../songs/1.mp3');
+let masterPlayLink = document.getElementById('masterPlayLink');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Warriyo - Mortals [NCS Release]", filePath: "../songs/1.mp3", coverPath: "../img/covers/1.jpg"},
+    {songName: "Cielo - Huma-Huma", filePath: "../songs/2.mp3", coverPath: "../img/covers/2.jpg"},
+    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "../songs/3.mp3", coverPath: "../img/covers/3.jpg"},
+    {songName: "Different Heaven & EH!DE - My Heart", filePath: "../songs/4.mp3", coverPath: "../img/covers/4.jpg"},
+    {songName: "Janji-Heroes-Tonight-feat-Johnning", filePath: "../songs/5.mp3", coverPath: "../img/covers/5.jpg"},
+    {songName: "No Name - Ft. [NCS Release]-320k", filePath: "../songs/2.mp3", coverPath: "../img/covers/6.jpg"},
+    {songName: "Back It - [NCS Release]-320k", filePath: "../songs/2.mp3", coverPath: "../img/covers/7.jpg"},
+]
+
+// alert(audioElement.duration);
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+
+
+// Handle play/pause click
+document.getElementById('masterPlayLink').addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        document.getElementById('masterPlay').classList.remove('fa-circle-play');
+        document.getElementById('masterPlay').classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        document.getElementById('masterPlay').classList.remove('fa-circle-pause');
+        document.getElementById('masterPlay').classList.add('fa-circle-play');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-circle-pause');
+        element.classList.add('fa-circle-play');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
+        audioElement.src = `../songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+    })
+})
+
+
+
+document.getElementById('nextLink').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previousLink').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+
+
+
+
